af630a42797c0db54a6111ea4604807e04db1c16
KokSiang <apache088@gmail.com>
2025-06-16 17:10:51 +0800

    added new columns in price_plans table - price_plan_id, valid_from, valid_to; updated schema design, model creation codes and tests

--- citibike_dbt/models/fact_trips.sql
+++ citibike_dbt/models/fact_trips.sql
@@ -7,17 +7,20 @@ SELECT
     CAST(end_stations.station_key AS STRING) AS end_station_key,
     CAST(membership_types.membership_type_id AS STRING) AS membership_type_id,
     CAST(bike_types.bike_type_id AS STRING) AS bike_type_id,
-    CAST((
-        COALESCE(price_plans.unlock_fee, 0) +
-        COALESCE(
-            CASE
-                WHEN CAST(TIMESTAMP_DIFF(CAST(ended_at AS TIMESTAMP), CAST(started_at AS TIMESTAMP), MINUTE) AS INT64) > COALESCE(price_plans.included_mins, 0)
-                THEN (CAST(TIMESTAMP_DIFF(CAST(ended_at AS TIMESTAMP), CAST(started_at AS TIMESTAMP), MINUTE) AS INT64) - COALESCE(price_plans.included_mins, 0)) * COALESCE(price_plans.per_minute_pricing, 0)
-                ELSE 0
-            END,
-            0
-        )
-    ) AS FLOAT64) AS price_paid
+    CAST(price_plans.price_plan_id AS INT64) AS price_plan_id,
+    ROUND(
+        CAST((
+            COALESCE(price_plans.unlock_fee, 0) +
+            COALESCE(
+                CASE
+                    WHEN CAST(TIMESTAMP_DIFF(CAST(ended_at AS TIMESTAMP), CAST(started_at AS TIMESTAMP), MINUTE) AS INT64) > COALESCE(price_plans.included_mins, 0)
+                    THEN (CAST(TIMESTAMP_DIFF(CAST(ended_at AS TIMESTAMP), CAST(started_at AS TIMESTAMP), MINUTE) AS INT64) - COALESCE(price_plans.included_mins, 0)) * COALESCE(price_plans.per_minute_pricing, 0)
+                    ELSE 0
+                END,
+                0
+            )
+        ) AS FLOAT64),
+    2) AS price_paid
 FROM
     {{ source('citibike_ingestion', 'main_citibike_tripdata') }} source_data
 INNER JOIN {{ ref('dim_stations') }} start_stations
@@ -37,3 +40,4 @@ INNER JOIN {{ ref('dim_bike_types') }} bike_types
 INNER JOIN {{ ref('price_plans') }} price_plans
     ON IFNULL(source_data.member_casual, '') = IFNULL(price_plans.membership_type, '')
     AND IFNULL(source_data.rideable_type, '') = IFNULL(price_plans.bike_type, '')
+    AND DATE(CAST(source_data.started_at AS TIMESTAMP)) BETWEEN price_plans.valid_from AND price_plans.valid_to
