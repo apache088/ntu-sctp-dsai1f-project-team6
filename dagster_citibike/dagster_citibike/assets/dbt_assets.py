86d228986457a7790f34fdfd2de4d5253e9b97e0
KokSiang <61038658+apache088@users.noreply.github.com>
2025-06-18 22:53:27 +0800

    Delete dagster_citibike directory

--- dagster_citibike/dagster_citibike/assets/dbt_assets.py
+++
@@ -1,7 +0,0 @@
-from dagster_dbt import dbt_assets, DbtCliResource
-from dagster_citibike.resources import dbt_manifest_path
-
-@dbt_assets(manifest=dbt_manifest_path)
-def transform_bigquery_data(dbt: DbtCliResource):
-    # Run dbt build
-    yield from dbt.cli(["build"]).stream()  
--- dagster_citibike/dagster_citibike/jobs.py
+++
@@ -1,9 +0,0 @@
-from dagster import define_asset_job, AssetSelection
-from dagster_citibike.assets import dbt_assets
-
-# Define a job that runs Meltano → dbt
-elt_job = define_asset_job(
-    name="elt_job",
-    selection=AssetSelection.groups("meltano_github_json") 
-    | AssetSelection.from_modules(dbt_assets)
-)
