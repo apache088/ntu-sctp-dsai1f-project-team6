f5780c77a74e743554513cb23a1d4be2a6f0a574
Xiangrong <goddino@gmail.com>
2025-06-19 23:54:52 +0800

    ENH: Only 1 .env at project root is required. - Script is now used solely to load .env variables into the environment.

---
+++ load_env.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from dotenv import load_dotenv
+
+# Load .env variables to the environment.
+# Looks for a .env file in the dir of this script or searches for it incrementally higher up.
+load_dotenv()
